3aeda93a250d5db6026e2e0c5d58d48bbbd725ac
Rupertofly <rupertqmoore@me.com>
2018-06-03 21:10:13 +1000

    just Checking firebased worked it's all g

--- src/sketch/main.js
+++ src/sketch/main.js
@@ -1,12 +1,12 @@
 import getC from './pallete';
-import * as fb from 'firebase';
 import { getRandomInt } from './helperFuncs';
+import firebase from '../../node_modules/firebase/index';
 
 export function preload() {}
-
 export function setup() {
   createCanvas( window.innerWidth, window.innerHeight );
   background( getC( getRandomInt( 0, 12 ), getRandomInt( 2, 5 ) ).hex );
+  console.log( firebase );
 }
 
 export function draw() {}
