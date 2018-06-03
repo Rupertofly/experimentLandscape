299141bec072d9059d6cbfa9bed805ded629245f
Rupertofly <rupertqmoore@me.com>
2018-06-03 20:55:47 +1000

    cleaned up some final things

--- src/sketch/main.js
+++ src/sketch/main.js
@@ -1,48 +1,14 @@
-import interpolate from 'b-spline';
-import { getRandomInt } from './helperFuncs';
 import getC from './pallete';
+import * as fb from 'firebase';
+import { getRandomInt } from './helperFuncs';
 
-let x, y, backgroundColor, esp;
-
-const width = window.innerWidth;
-const height = window.innerHeight;
+export function preload() {}
 
 export function setup() {
-  console.log( interpolate );
-  createCanvas( width, height, WEBGL );
-  backgroundColor = color( getC( getRandomInt( 0, 6 ), getRandomInt( 0, 6 ) ).hex );
-
-  x = random( width );
-  y = height / 2;
-  let vor = d3.voronoi().size( [width, height] );
-  esp = vor(
-    d3.range( 300 ).map( () => [random( width ), random( height )] )
-  ).polygons();
+  createCanvas( window.innerWidth, window.innerHeight );
+  background( getC( getRandomInt( 0, 12 ), getRandomInt( 2, 5 ) ).hex );
 }
 
-export function draw() {
-  translate( -width / 2, -height / 2 );
-  background( backgroundColor );
-  fill( color( 255, 0, 0 ) );
-  noStroke();
-  directionalLight( 255, 0, 0, width / 2 + x, 50, -20 );
-  ambientLight( 50, 30, 30 );
-  push();
-  translate( x, y, -6 );
-  ambientMaterial( 255 );
-  sphere( 120 );
-  pop();
-  x = ( x + 1 ) % width;
-  noFill();
-  stroke( 0 );
-  strokeWeight( 5 );
-  esp.map( poly => {
-    beginShape();
-    poly.map( point => vertex( point[0], point[1] ) );
-    endShape( CLOSE );
-  } );
-}
+export function draw() {}
 
-export function mousePressed() {
-  backgroundColor = color( getC( getRandomInt( 0, 6 ), getRandomInt( 0, 6 ) ).hex );
-}
+export function mousePressed() {}
